ed7140cfb5af9e0d4f6135ebe25a3d703b92e5cd
MartinJeanne <martinjeanne.dev@gmail.com>
2023-09-05 18:16:26 +0200

    SubMain is going pretty well!

--- src/components/SubMain.jsx
+++ src/components/SubMain.jsx
@@ -2,6 +2,11 @@ import { useState } from 'react'
 import RoundImg from './RoundImg.jsx'
 import '../style/subMain.css'
 import greyBg from '../assets/subMainGreyBg.png'
+import man1 from '../assets/persona/man1.png'
+import man2 from '../assets/persona/man2.png'
+import woman1 from '../assets/persona/woman1.png'
+import woman2 from '../assets/persona/woman2.png'
+
 
 function SubMain() {
   /*const [bg, setBg] = useState('white');
@@ -11,8 +16,6 @@ function SubMain() {
     else setBg('white');
   }*/
 
-  const src = "https://www.pngall.com/wp-content/uploads/2016/03/Rat-PNG-Picture.png";
-
   var subMainStyle = {
     backgroundImage: `url(${greyBg})`,
     backgroundPosition: 'center',
@@ -21,27 +24,31 @@ function SubMain() {
 
   return (
     <div id='subMain' style={subMainStyle}>
-      <div className='profilPictures'>
-        <div className='profilPictureLine'>
-          <RoundImg src={src} />
+      <h1 className='subMainTitle'>Join thousand of user!</h1>
+
+      <div id='subMainContent'>
+        <div className='profilPictures'>
+          <div className='profilPictureLine'>
+            <RoundImg src={man1} />
+          </div>
+          <div className='profilPictureLine'>
+            <RoundImg src={man2} />
+            <RoundImg src={woman1} />
+          </div>
+          <div className='profilPictureLine'>
+            <RoundImg src={woman2} />
+          </div>
         </div>
-        <div className='profilPictureLine'>
-          <RoundImg src={src} />
-          <RoundImg src={src} />
-        </div>
-        <div className='profilPictureLine'>
-          <RoundImg src={src} />
-        </div>
-      </div>
-      <div className='subMainPlaylists'>
-        <h1>Gazoil</h1>
-        <p>Playlist saved:</p>
-        <div className='profilPictureLine'>
-          <RoundImg src={src} />
-        </div>
-        <div className='profilPictureLine'>
-          <RoundImg src={src} />
-          <RoundImg src={src} />
+        <div className='subMainPlaylists'>
+          <h1>Gazoil</h1>
+          <p>Playlist saved:</p>
+          <div className='profilPictureLine'>
+            <RoundImg src={man1} />
+          </div>
+          <div className='profilPictureLine'>
+            <RoundImg src={man1} />
+            <RoundImg src={man1} />
+          </div>
         </div>
       </div>
     </div>
